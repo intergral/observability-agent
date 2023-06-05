cae1bca64551ac6a1b6bead9694e76958832a082
callum.yuille <callum.yuille@intergral.com>
2023-06-05 17:18:06 +0100

    fixed adding multiple postgres data sources

--- linux/observability-agent-autoconf.sh
+++ linux/observability-agent-autoconf.sh
@@ -499,13 +499,10 @@ if (ss -ltn | grep -qE :5432) || [ -n "${postgres_connection_string}" ]; then
       echo "Postgres credentials not found"
     fi
   else
-    IFS=, read -ra connection_strings <<< "$postgres_connection_string"
+    IFS=", " read -ra connection_strings <<< "$postgres_connection_string"
     data_sources=$(IFS=, ; printf '"%s", ' "${connection_strings[@]}")
     data_sources=${data_sources%, }  # Remove trailing comma and space
-    #yq -i e '.integrations.postgres_exporter.enabled |= true, .integrations.postgres_exporter.data_source_names |= ['"$data_sources"'], .integrations.postgres_exporter.autodiscover_databases |= true' "$CONFIG"
-#    yq -i e '.integrations.postgres_exporter.enabled |= true, .integrations.postgres_exporter.data_source_names |= ['"$data_sources"']' "$CONFIG"
-#    yq -i e '.integrations.postgres_exporter.autodiscover_databases |= true' "$CONFIG"
-    yq -i e '(.integrations.postgres_exporter.enabled |= true, .integrations.postgres_exporter.data_source_names |= ['"$data_sources"']) | (.integrations.postgres_exporter.autodiscover_databases |= true)' "$CONFIG"
+    yq -i e '(.integrations.postgres_exporter.enabled |= true, .integrations.postgres_exporter.data_source_names |= ['"$data_sources"']) | (.integrations.postgres_exporter.autodiscover_databases |= true) | .integrations.postgres_exporter.data_source_names[] style="double"' "$CONFIG"
 
   fi
   if [ -n "${postgres_disabled}" ] && [ "${postgres_disabled}" = true ]; then
